c97971a3ee4fe25bec1e608f15b6e1326a8b6d79
shovan777 <shovan@fusemachines.com>
2017-12-20 06:16:57 +0545

    Add til for dec 17 and 18

--- src/mysql_db/UserDao.java
+++
@@ -1,8 +0,0 @@
-package mysql_db;
-
-import java.util.ArrayList;
-
-public interface UserDao {
-	ArrayList<User> getUserByLNameWIndex(String lastName);
-	ArrayList<User> getUserByLNameWOIndex(String lastName);
-}
